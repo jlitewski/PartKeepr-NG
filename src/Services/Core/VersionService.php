e35756629ccf581290d37a366667df589097e2e1
Jacob Litewski <jacob.litewski@gmail.com>
2021-08-23 17:27:41 -0400

    Flesh out the logic a bit in VersionService

--- src/Services/Core/VersionService.php
+++ src/Services/Core/VersionService.php
@@ -43,4 +43,69 @@ class VersionService {
     }
 
     //TODO: Implement the functions this class needs to function
+    /**
+     * Extracts the current commit from GIT.
+     * 
+     * @param bool $short If we should pass '--short' to the command
+     * @return string The result of the command
+     */
+    public function extractGITCommit(bool $short = false): string {
+        $cmd = "git rev-parse ";
+        if($short) { $cmd .= "--short "; }
+        $cmd .= "HEAD";
+
+        return trim(shell_exec($cmd));
+    }
+
+    /**
+     * Sets the version string. Replaces the default set in services.yaml (app.version)
+     * 
+     * @param string $version The version string to replace the default one with
+     * @return self
+     */
+    public function setVersion(string $version): self {
+        $this->version = $version;
+        return $this;
+    }
+
+    /**
+     * 
+     */
+    public function getVersion(): string {
+        return $this->version;
+    }
+
+    /**
+     * 
+     */
+    public function setVersionURI(string $versionURI): self {
+        $this->versionURI = $versionURI;
+        return $this;
+    }
+
+    /**
+     * 
+     */
+    public function getCanonicalVersion(): string {
+        if($this->version === 'V_GIT') {
+            return 'GIT development version Commit '.$this->extractGITCommit()." Short Commit ".$this->extractGITCommit(true);
+        } else {
+            return $this->version;
+        }
+    }
+
+    /**
+     * 
+     */
+    public function doVersionCheck(): void {
+        //TODO: Flesh out the logic in this once we have a need for an updater
+    }
+
+    /**
+     * 
+     */
+    public function getLatestVersion(): mixed {
+        //TODO: Flesh out the logic in this once we have a need for an updater
+        return false; //XXX: Fail fast
+    }
 }
